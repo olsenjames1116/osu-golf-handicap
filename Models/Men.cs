07b98a379dc19068a9551b062002aff445fe3adc
James Olsen <olsen.159@osu.edu>
2018-04-12 13:34:16 -0400

    added models

---
+++ Models/Men.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace osu.golf.handicap.Models
+{
+    public class Men
+    {
+        public int score{get; set;}
+        public double courseRating{get; set;}
+        public double slopeRating{get; set;}
+    }
+}
---
+++ Models/Women.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace osu.golf.handicap.Models
+{
+    public class Women
+    {
+        public int courseID{get; set;}
+        public string courseName{get; set;}
+        public int score{get; set;}
+        public double courseRating{get; set;}
+        public double slopeRating{get; set;}
+   }
+}
