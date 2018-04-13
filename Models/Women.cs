f9384921d22af492e2f3d0dbf8239b4aea9b44b6
James Olsen <olsen.159@osu.edu>
2018-04-12 21:11:34 -0400

    added calculator

--- Models/Women.cs
+++ Models/Women.cs
@@ -6,8 +6,13 @@ namespace osu.golf.handicap.Models
     {
         public int courseID{get; set;}
         public string courseName{get; set;}
-        public int score{get; set;}
+        public int score1{get; set;}
+        public int score2{get; set;}
+        public int score3{get; set;}
+        public int score4{get; set;}
+        public int score5{get; set;}
         public double courseRating{get; set;}
         public double slopeRating{get; set;}
+        public double handicap{get; set;}
    }
 }
